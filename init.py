ad41af1438281ed6b0fe6035210498af9615ab75
Billel-ABBAS <abbas.billel1985@gmail.com>
2024-08-06 13:01:43 +0200

    update version 3

--- init.py
+++ init.py
@@ -606,6 +606,8 @@ def calculate_similarity_matrix(M_test_quest_topics, M_train_quest_topics):
     return similarity_matrix
 
 
+################################ Partie teste non supervisé dans le notebook ################################
+
 # Définition de la fonction de prédiction
 def predict_keywords(new_text, lda_model, nmf_model, vectorizer, train_topics_lda, train_topics_nmf, tags_train, vectorizer_tags):
     
@@ -644,3 +646,81 @@ def predict_keywords(new_text, lda_model, nmf_model, vectorizer, train_topics_ld
     predicted_semi_supervised_keywords_nmf = df_new_semi_supervised_nmf.iloc[0].nlargest(5).index.tolist()
     
     return predicted_keywords_lda, predicted_semi_supervised_keywords_lda, predicted_keywords_nmf, predicted_semi_supervised_keywords_nmf
+
+
+
+############################################ Partie teste supervisé dans le notebook #############################
+
+
+
+
+
+
+# ############################################ Partie API #####################################
+# Fonction pour charger les modèles et les données
+def load_models_and_data():
+    with open('../Model/unsupervised/lda_model.pkl', 'rb') as file:
+        lda_model = pickle.load(file)
+
+    with open('../Model/unsupervised/nmf_model.pkl', 'rb') as file:
+        nmf_model = pickle.load(file)
+
+    with open('../Model/unsupervised/vectorizer.pkl', 'rb') as file:
+        vectorizer = pickle.load(file)
+
+    with open('../Model/unsupervised/vectorizer_tags.pkl', 'rb') as file:
+        vectorizer_tags = pickle.load(file)
+
+    with open('../Model/unsupervised/train_topics_lda.pkl', 'rb') as file:
+        train_topics_lda = pickle.load(file)
+
+    with open('../Model/unsupervised/train_topics_nmf.pkl', 'rb') as file:
+        train_topics_nmf = pickle.load(file)
+
+    # Charger la matrice des tags depuis le fichier compressé NPZ
+    file_path_tags = '../Model/unsupervised/tags_train.npz'
+    with np.load(file_path_tags) as data:
+        tags_train = data['tags_train']
+
+    return lda_model, nmf_model, vectorizer, vectorizer_tags, train_topics_lda, train_topics_nmf, tags_train
+
+
+# # Définition de la fonction de prédiction
+# def predict_keywords_api(new_text, lda_model, nmf_model, vectorizer, train_topics_lda, train_topics_nmf, tags_train, vectorizer_tags):
+    
+#     # Prétraitement du texte
+#     new_bow = vectorizer.transform([new_text])
+    
+#     # Prédiction avec LDA
+#     new_topics_lda = lda_model.transform(new_bow)
+#     new_words_lda = calculate_words(new_topics_lda, lda_model)
+#     new_words_filtered_lda = filter_words(new_words_lda, threshold=0)
+    
+#     # Prédiction avec NMF
+#     new_topics_nmf = nmf_model.transform(new_bow)
+#     new_words_nmf = calculate_words(new_topics_nmf, nmf_model)
+#     new_words_filtered_nmf = filter_words(new_words_nmf, threshold=0)
+    
+#     # # Prédiction semi-supervisée
+#     # similarity_matrix_lda = calculate_similarity_matrix(new_topics_lda, train_topics_lda)
+#     # semi_supervised_keywords_lda = np.dot(similarity_matrix_lda, tags_train)
+#     # similarity_matrix_nmf = calculate_similarity_matrix(new_topics_nmf, train_topics_nmf)
+#     # semi_supervised_keywords_nmf = np.dot(similarity_matrix_nmf, tags_train)
+    
+#     # Conversion en DataFrame pour visualisation
+#     words = vectorizer.get_feature_names_out()
+#     tags = vectorizer_tags.get_feature_names_out()
+    
+#     df_new_words_lda = pd.DataFrame(new_words_filtered_lda, columns=words)
+#     # df_new_semi_supervised_lda = pd.DataFrame(semi_supervised_keywords_lda, columns=tags)
+#     df_new_words_nmf = pd.DataFrame(new_words_filtered_nmf, columns=words)
+#     # df_new_semi_supervised_nmf = pd.DataFrame(semi_supervised_keywords_nmf, columns=tags)
+    
+#     # Sélection des 5 mots clés les plus pertinents pour chaque prédiction
+#     predicted_keywords_lda = df_new_words_lda.iloc[0].nlargest(5).index.tolist()
+#     # predicted_semi_supervised_keywords_lda = df_new_semi_supervised_lda.iloc[0].nlargest(5).index.tolist()
+#     predicted_keywords_nmf = df_new_words_nmf.iloc[0].nlargest(5).index.tolist()
+#     # predicted_semi_supervised_keywords_nmf = df_new_semi_supervised_nmf.iloc[0].nlargest(5).index.tolist()
+    
+#     return predicted_keywords_lda, predicted_semi_supervised_keywords_lda, predicted_keywords_nmf, predicted_semi_supervised_keywords_nmf
+
---
+++ streamlit/api_streamlit.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import sys
+import os
+
+
+# Ajouter le répertoire parent au chemin de recherche de modules
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Importations des fonctions personnalisées
+import init as func
+
+
+
+# Interface utilisateur avec Streamlit
+st.title('Prédiction de mots-clés avec LDA et NMF')
+
+# Entrée de texte par l'utilisateur
+user_input = st.text_area("Entrez le texte pour la prédiction des mots-clés:")
+
+if st.button('Prédire'):
+    if user_input:
+        try:
+            # Nettoyage et tokenisation du texte d'entrée
+            cleaned_html_input = func.clean_html(user_input)
+            cleaned_input = func.process_clean_text(cleaned_html_input)
+            
+            # Charger les modèles et les données
+            lda_model, nmf_model, vectorizer, vectorizer_tags, train_topics_lda, train_topics_nmf, tags_train = func.load_models_and_data() 
+            
+            # Prédiction des mots-clés avec LDA et NMF
+            predicted_keywords_lda, predicted_semi_supervised_keywords_lda, predicted_keywords_nmf, predicted_semi_supervised_keywords_nmf = func.predict_keywords(
+                cleaned_input, lda_model, nmf_model, vectorizer, train_topics_lda, train_topics_nmf, tags_train, vectorizer_tags
+            )
+            
+            st.subheader('Texte nettoyé:')
+            st.write(cleaned_input)
+            
+            st.subheader('Mots-clés prédits avec le modèle LDA:')
+            st.write(predicted_keywords_lda)
+            
+            st.subheader('Mots-clés semi-supervisés prédits avec le modèle LDA:')
+            st.write(predicted_semi_supervised_keywords_lda)
+            
+            st.subheader('Mots-clés prédits avec le modèle NMF:')
+            st.write(predicted_keywords_nmf)
+            
+            st.subheader('Mots-clés semi-supervisés prédits avec le modèle NMF:')
+            st.write(predicted_semi_supervised_keywords_nmf)
+            
+        except Exception as e:
+            st.error(f"Erreur lors de la prédiction: {str(e)}")
+    else:
+        st.error("Veuillez entrer du texte pour la prédiction.")
+
