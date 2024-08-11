3dc86d096a8fadc5ba2045ec9abbb0ad5f5c3afd
Billel-ABBAS <abbas.billel1985@gmail.com>
2024-08-12 01:52:53 +0200

    update version 47

--- streamlit/api_streamlit.py
+++ streamlit/api_streamlit.py
@@ -5,6 +5,10 @@ import mlflow.pyfunc
 import sys
 import os
 
+# Récupérer les identifiants AWS depuis les variables d'environnement
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+
 # Ajouter le répertoire parent au PYTHONPATH
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
