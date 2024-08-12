ec4f2419819e32e94549c4a7ca1fde3dd2276c6a
Billel-ABBAS <abbas.billel1985@gmail.com>
2024-08-12 02:52:57 +0200

    update version 53

--- streamlit/api_streamlit.py
+++ streamlit/api_streamlit.py
@@ -54,7 +54,7 @@ mlb = load_mlflow_model(logged_mlb, 'sklearn')
 
 # Charger le modèle XGBoost en tant que PyFuncModel depuis MLflow
 logged_model = 'runs:/a4ead1bae4f9424bb0ec41236e5bd50d/XGBoost'
-best_model = load_mlflow_model(logged_model, 'sklearn')
+best_model = load_mlflow_model(logged_model, 'pyfunc')
 #   if best_model is not None:
 #       st.success("Meilleur modèle XGBoost chargé avec succès!")
 
