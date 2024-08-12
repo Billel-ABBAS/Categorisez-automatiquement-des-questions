2e1684be3a81da285810b0678fb5f74af49f14b0
Billel-ABBAS <abbas.billel1985@gmail.com>
2024-08-12 16:44:28 +0200

    update version 86

--- streamlit/api_streamlit.py
+++ streamlit/api_streamlit.py
@@ -9,8 +9,8 @@ import sys
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-# # Ajouter le répertoire parent au PYTHONPATH
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Ajouter le répertoire parent au PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Importation des modules utilitaires personnalisés
 from utils import utils_exploratoire as func_exploratoire
